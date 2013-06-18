371284b9b9914faca1dacdf72df8de28f4becbe8
Ben Macdonell <benjamin.macdonell@gmail.com>
2013-06-18 11:45:12 -0700

    adding stuff from yesterday

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,6 +30,12 @@ describe "Static pages" do
     end
   end
 
+  it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end
+
   describe "About page" do
 
     it "should have the h1 'About Us'" do
